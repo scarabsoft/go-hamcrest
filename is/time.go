7689734f5cfeae0a587b4a03e1672cc4d9df2f10
Dominique Dymke <dominique@dymke.net>
2021-07-08 14:17:53 +0700

    implemented time

--- is/time.go
+++ is/time.go
@@ -1,53 +1,72 @@
 package is
 
 import (
+	"fmt"
+	"github.com/scarabsoft/go-hamcrest/internal"
 	"github.com/scarabsoft/go-hamcrest/matcher"
+	"time"
 )
 
 func Before(given interface{}) matcher.Matcher {
-	return matcher.New(
-		func(actual interface{}, chain matcher.Chain) matcher.Chain {
-			panic("Implement me")
-		},
-	)
-}
-
-func BeforeOrEqual(given interface{}) matcher.Matcher {
-	return matcher.New(
-		func(actual interface{}, chain matcher.Chain) matcher.Chain {
-			panic("Implement me")
-		},
-	)
+	return timeCheck(given, internal.IsLessThan, "want value before %s; got %s")
 }
 
 func NotBefore(given interface{}) matcher.Matcher {
-	return matcher.New(
-		func(actual interface{}, chain matcher.Chain) matcher.Chain {
-			panic("Implement me")
-		},
-	)
+	return timeCheck(given, func(actual, given interface{}) bool {
+		return internal.IsGreaterThanEqual(actual, given)
+	}, "want value not before %s; got %s")
+}
+
+func BeforeOrEqual(given interface{}) matcher.Matcher {
+	return timeCheck(given, internal.IsLessThanEqual, "want value before or equal to %s; got %s")
 }
 
 func After(given interface{}) matcher.Matcher {
-	return matcher.New(
-		func(actual interface{}, chain matcher.Chain) matcher.Chain {
-			panic("Implement me")
-		},
-	)
-}
-
-func AfterOrEqual(given interface{}) matcher.Matcher {
-	return matcher.New(
-		func(actual interface{}, chain matcher.Chain) matcher.Chain {
-			panic("Implement me")
-		},
-	)
+	return timeCheck(given, internal.IsGreaterThan, "want value after %s; got %s")
 }
 
 func NotAfter(given interface{}) matcher.Matcher {
+	return timeCheck(given, internal.IsLessThanEqual, "want value not after %s; got %s")
+}
+
+func AfterOrEqual(given interface{}) matcher.Matcher {
+	return timeCheck(given, internal.IsGreaterThanEqual, "want value after or equal to %s; got %s")
+}
+
+func failIfNotTimeType(name string, value interface{}, ) matcher.Function {
+	return func() matcher.MatchResult {
+
+		_, ok := value.(time.Time)
+		if !ok {
+			return matcher.Failed(fmt.Sprintf("want %s to be type of time.Time; got %s", name, internal.FormatTypeWithValue(value)))
+		}
+
+		return matcher.Next()
+	}
+}
+
+func timeCheck(given interface{}, fn func(interface{}, interface{}) bool, format string) matcher.Matcher {
 	return matcher.New(
 		func(actual interface{}, chain matcher.Chain) matcher.Chain {
-			panic("Implement me")
+			return chain.
+				Add(matcher.FailIfIsNil("actual", actual)).
+				Add(matcher.FailIfIsNil("given", given)).
+				Add(failIfNotTimeType("actual", actual)).
+				Add(failIfNotTimeType("given", given)).
+				Add(func() matcher.MatchResult {
+					actualTime := actual.(time.Time)
+					givenTime := given.(time.Time)
+
+					if !fn(actualTime.UnixNano(), givenTime.UnixNano()) {
+						return matcher.Failed(
+							fmt.Sprintf(format,
+								givenTime.Format(time.RFC3339Nano),
+								actualTime.Format(time.RFC3339Nano),
+							),
+						)
+					}
+					return matcher.Matched()
+				})
 		},
 	)
 }
--- is/time_test.go
+++ is/time_test.go
@@ -1,27 +1,370 @@
 package is_test
 
-import "testing"
+import (
+	"github.com/scarabsoft/go-hamcrest"
+	"github.com/scarabsoft/go-hamcrest/is"
+	"testing"
+	"time"
+)
+
+func createTime(v string) time.Time {
+	result, err := time.Parse(time.RFC3339, v)
+	if err != nil {
+		panic(err)
+	}
+	return result
+}
+
+var (
+	someTime       = createTime("2012-11-02T22:08:41+00:00")
+	beforeThatTime = createTime("2012-11-01T22:08:41+00:00")
+	afterThatTime  = createTime("2012-11-03T22:08:41+00:00")
+)
 
 func TestBefore(t *testing.T) {
-	t.Errorf("Implement me")
+	t.Run("nil_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.Before(someTime)
+		matched := matcher.Matches(nil)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("actual is <nil>"))
+	})
+
+	t.Run("someTime_nil", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.Before(nil)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("given is <nil>"))
+	})
+
+	t.Run("int_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.Before(someTime)
+		matched := matcher.Matches(10)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want actual to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("someTime_int", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.Before(10)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want given to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("beforeThatTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(beforeThatTime, is.Before(someTime))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
+
+	t.Run("someTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.Before(someTime)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want value before 2012-11-02T22:08:41Z; got 2012-11-02T22:08:41Z"))
+	})
 }
 
 func TestNotBefore(t *testing.T) {
-	t.Errorf("Implement me")
+	t.Run("nil_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotBefore(someTime)
+		matched := matcher.Matches(nil)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("actual is <nil>"))
+	})
+
+	t.Run("someTime_nil", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotBefore(nil)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("given is <nil>"))
+	})
+
+	t.Run("int_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotBefore(someTime)
+		matched := matcher.Matches(10)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want actual to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("someTime_int", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotBefore(10)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want given to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("beforeThatTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotBefore(someTime)
+		matched := matcher.Matches(beforeThatTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want value not before 2012-11-02T22:08:41Z; got 2012-11-01T22:08:41Z"))
+	})
+
+	t.Run("someTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(someTime, is.NotBefore(someTime))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
 }
 
 func TestBeforeOrEqual(t *testing.T) {
-	t.Errorf("Implement me")
+	t.Run("nil_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.BeforeOrEqual(someTime)
+		matched := matcher.Matches(nil)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("actual is <nil>"))
+	})
+
+	t.Run("someTime_nil", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.BeforeOrEqual(nil)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("given is <nil>"))
+	})
+
+	t.Run("int_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.BeforeOrEqual(someTime)
+		matched := matcher.Matches(10)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want actual to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("someTime_int", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.BeforeOrEqual(10)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want given to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("BeforeOrSameThatTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(beforeThatTime, is.BeforeOrEqual(someTime))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
+
+	t.Run("someTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(beforeThatTime, is.BeforeOrEqual(someTime))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
 }
 
 func TestAfter(t *testing.T) {
-	t.Errorf("Implement me")
-}
+	t.Run("nil_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
 
-func TestAfterOrEqual(t *testing.T) {
-	t.Errorf("Implement me")
+		matcher := is.After(someTime)
+		matched := matcher.Matches(nil)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("actual is <nil>"))
+	})
+
+	t.Run("someTime_nil", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.After(nil)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("given is <nil>"))
+	})
+
+	t.Run("int_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.After(someTime)
+		matched := matcher.Matches(10)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want actual to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("someTime_int", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.After(10)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want given to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("afterThatTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(afterThatTime, is.After(someTime))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
+
+	t.Run("someTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.After(someTime)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want value after 2012-11-02T22:08:41Z; got 2012-11-02T22:08:41Z"))
+	})
 }
 
 func TestNotAfter(t *testing.T) {
-	t.Errorf("Implement me")
+	t.Run("nil_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotAfter(someTime)
+		matched := matcher.Matches(nil)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("actual is <nil>"))
+	})
+
+	t.Run("someTime_nil", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotAfter(nil)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("given is <nil>"))
+	})
+
+	t.Run("int_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotAfter(someTime)
+		matched := matcher.Matches(10)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want actual to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("someTime_int", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotAfter(10)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want given to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("afterThatTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotAfter(someTime)
+		matched := matcher.Matches(afterThatTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want value not after 2012-11-02T22:08:41Z; got 2012-11-03T22:08:41Z"))
+	})
+
+	t.Run("someTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(someTime, is.NotAfter(someTime))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
+}
+
+func TestAfterOrEqual(t *testing.T) {
+	t.Run("nil_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.AfterOrEqual(someTime)
+		matched := matcher.Matches(nil)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("actual is <nil>"))
+	})
+
+	t.Run("someTime_nil", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.AfterOrEqual(nil)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("given is <nil>"))
+	})
+
+	t.Run("int_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.AfterOrEqual(someTime)
+		matched := matcher.Matches(10)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want actual to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("someTime_int", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.AfterOrEqual(10)
+		matched := matcher.Matches(someTime)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("want given to be type of time.Time; got int(10)"))
+	})
+
+	t.Run("afterThatTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(afterThatTime, is.AfterOrEqual(someTime))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
+
+	t.Run("someTime_someTime", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(someTime, is.AfterOrEqual(someTime))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
 }
