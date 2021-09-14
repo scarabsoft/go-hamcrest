5fbed5275671a1d2cc99bffac5c9b7bb33176909
Dominique Dymke <dominique@dymke.net>
2021-09-14 12:19:36 +0200

    added NotPointingTo

--- is/pointer.go
+++ is/pointer.go
@@ -32,3 +32,29 @@ func PointingTo(given interface{}) matcher.Matcher {
 				})
 	})
 }
+
+func NotPointingTo(given interface{}) matcher.Matcher {
+	return matcher.New(func(actual interface{}, chain matcher.Chain) matcher.Chain {
+		return chain.
+			Add(
+				matcher.MatchIfAllAreNil(actual, given),
+				matcher.FailIfIsNil("actual", actual),
+				matcher.FailIfIsNil("given", given),
+				matcher.FailIfNotRestrictedType("actual", actual, internal.RestrictedToPointer),
+				matcher.FailIfNotRestrictedType("given", given, internal.RestrictedToPointer),
+				func() matcher.MatchResult {
+					actualAddress := reflect.ValueOf(actual).Pointer()
+					givenAddress := reflect.ValueOf(given).Pointer()
+
+					if internal.IsEqual(actualAddress, givenAddress) {
+						return matcher.Failed(fmt.Sprintf(
+							"want pointer to not point to %s; got %s",
+							internal.FormatTypeWithValue(actual),
+							internal.FormatTypeWithValue(given),
+						))
+					}
+
+					return matcher.Matched()
+				})
+	})
+}
--- is/pointer_test.go
+++ is/pointer_test.go
@@ -101,3 +101,100 @@ func TestPointingTo(t *testing.T) {
 		assert.That(matcher.Cause(), is.MatchingPattern(`to\s\*float32\([a-zA-z0-9]*\);\sgot\s\*int\([a-zA-z0-9]*\)`))
 	})
 }
+
+func TestNotPointingTo(t *testing.T) {
+	var givenPtr = new(int)
+	*givenPtr = 10
+
+	t.Run("nil_nil", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := assert.That(nil, is.NotPointingTo(nil))
+		assert.That(matcher.Cause(), is.EqualTo(""))
+	})
+
+	t.Run("ptr_nil", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotPointingTo(nil)
+		matched := matcher.Matches(givenPtr)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("given is <nil>"))
+	})
+
+	t.Run("nil_ptr", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotPointingTo(givenPtr)
+		matched := matcher.Matches(nil)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("actual is <nil>"))
+	})
+
+	t.Run("1_ptr", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotPointingTo(givenPtr)
+		matched := matcher.Matches(1)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("actual not one of [ptr,unsafe.Pointer]"))
+	})
+
+	t.Run("ptr_1", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotPointingTo(1)
+		matched := matcher.Matches(givenPtr)
+
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.EqualTo("given not one of [ptr,unsafe.Pointer]"))
+	})
+
+	t.Run("same_ptr", func(t *testing.T) {
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotPointingTo(givenPtr)
+		matched := matcher.Matches(givenPtr)
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.MatchingPattern(`want\spointer\sto\snot\spoint\sto\s\*int\([a-zA-z0-9]*\);\sgot\s\*int\([a-zA-z0-9]*\)`))
+	})
+
+	t.Run("different_ptr_variables_same_address", func(t *testing.T) {
+		anotherPtr := givenPtr
+
+		assert := hamcrest.NewAssertion(t)
+
+		matcher := is.NotPointingTo(givenPtr)
+		matched := matcher.Matches(anotherPtr)
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.MatchingPattern(`want\spointer\sto\snot\spoint\sto\s\*int\([a-zA-z0-9]*\);\sgot\s\*int\([a-zA-z0-9]*\)`))
+
+		matcher = is.NotPointingTo(anotherPtr)
+		matched = matcher.Matches(givenPtr)
+		assert.That(matched, is.False())
+		assert.That(matcher.Cause(), is.MatchingPattern(`want\spointer\sto\snot\spoint\sto\s\*int\([a-zA-z0-9]*\);\sgot\s\*int\([a-zA-z0-9]*\)`))
+	})
+
+	t.Run("same_ptr_type_different_addresses", func(t *testing.T) {
+		var anotherPtr = new(int)
+		*anotherPtr = 10
+
+		assert := hamcrest.NewAssertion(t)
+
+		matched := assert.That(anotherPtr, is.NotPointingTo(givenPtr))
+		assert.That(matched.Cause(), is.EqualTo(""))
+	})
+
+	t.Run("different_types", func(t *testing.T) {
+		var anotherPtr = new(float32)
+		*anotherPtr = float32(42)
+
+		assert := hamcrest.NewAssertion(t)
+
+		matched := assert.That(anotherPtr, is.NotPointingTo(givenPtr))
+		assert.That(matched.Cause(), is.EqualTo(""))
+	})
+}
